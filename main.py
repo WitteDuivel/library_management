0f63c245dcd2d1b431d673ff8121a4f946cc19a3
WitteDuivel <77537702+WitteDuivel@users.noreply.github.com>
2021-03-07 18:53:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#NOTE
+
 #N ask the user if they are a staff member or a student
 #N verify their id accroding to what they are(staff_ID for staff members and stud_ID for students)
 #? FIRST FOR THE STUDENT USERS
@@ -26,59 +26,59 @@
 #N ask what do they want to update
 #N update table USERS accordigly
 stforstud=input("ARE YOU A STAFF MEMBER OR A STUDENT? (STAFF/STUDENT)")
-if stforstud=="STAFF":
+if stforstud.upper()=="STAFF":
     print("YOU ARE WELCOME!!!")
     neworold=input("ARE YOU A REGISTERED STAFF MEMBER? (YES/NO)")
-    if neworold=="YES":
+    if neworold.upper()=="YES":
         staff_ID=int(input("PLEASE ENTER YOUR STAFF_ID:- "))
         #TODO QUERY FOR CHECKING IF THE ID ENTERED EXISTS OR NOT
         #TODO IF EXISTS THEN AN IF STATEMENT TO VERIFY PASSWORD
         #TODO IF DOESN'T EXIST ASK IF THEY WANT TO REGISTER OR WANT TO LEAVE
         dataorbook=input("DO YOU WANT TO MANAGE YOUR DATA OR WANT TO MANAGE THE LIBRARY? (DATA/LIBRARY)")
-        if dataorbook=="DATA":
+        if dataorbook.upper()=="DATA":
             wholeorpart=input("DO YOU WANT TO CHANGE A PARTICULAR DATA OR WANT TO UPDATE YOUR WHOLE RECORDS? (SOME/WHOLE)")
-            if wholeorpart=="SOME":
+            if wholeorpart.upper()=="SOME":
                 which=input("WHICH OF THE FOLLOWING DATA YOU WANT TO UPDATE? ( staff_ID/stffname/stfflname/stffcontactnumber/stfemail/stfaddress/stfpassword/stftype)")
                 #TODO QUERY TO UPDATE THE CHOSEN RECORDS ACCRODINGLY
-            elif wholeorpart=="WHOLE":
+            elif wholeorpart.upper()=="WHOLE":
                 #TODO QUERY TO UPDATE THE WHOLE RECORD OF THE CURRENT USER
                 pass
             else:
                 print("WRONG INPUT")
-        elif dataorbook=="BOOK":
+        elif dataorbook.upper()=="BOOK":
             addormanage=input("DO YOU WANT TO ADD THE BOOK OR MANAGE THE DATA OF THE CURRENT BOOKS? (ADD/MANAGE)")
-            if addormanage=="ADD":
+            if addormanage.upper()=="ADD":
                 #TODO TO ADD A NEW BOOK IN THE DATABASE AND UPDATE THE TABLE BOOK
                 pass
-            elif addormanage=="MANAGE":
+            elif addormanage.upper()=="MANAGE":
                 #TODO QUERY TO MANAGE THE TABLE BOOK ACCRODING TO USERS NEED
                 pass
             else:
                 print("WRONG INPUT")
-    elif neworold=="NO":
+    elif neworold.upper()=="NO":
         registerornot=input("DO YOU WANT TO REGISTER AS A NEW STAFF MEMBER OF THIS LIBRARY? (YES/NO)")
-        if registerornot=="YES":
+        if registerornot.upper()=="YES":
             #TODO QUERY TO ASK FOR THE REQUIRED INFORMATION NEEDED TO REGISTER THE NEW STAFF MEMBER ACCORDING TO THE TABLE USERS
             pass
-        elif registerornot=="NO":
+        elif registerornot.upper()=="NO":
             print("TRY TO NOT WASTE TIME OF OURS AND OTHERS, THANK YOU!!!")
         else:
             print("WRONG INPUT, CHECK THE DATA AGAIN. POSSIBLE ERRORS MAY INCLUDE NOT SPELLING CORRECTLY OR DIFFERENT CASE OF THE LETTERS RATHER THAN CAPS. TRY AGAIN KEEPING THERE IN MIND")
     else:
         print("WRONG INPUT, CHECK AGAIN IF YOU HAVE SPELLT IT CORRECTLY AND IN THE UPPER CASE")
-elif stforstud=="STUDENT":
+elif stforstud.upper()=="STUDENT":
     print("WELCOME PRECIOUS FUTURE OF THIS EARTH :)")
     existingornot=input("ARE YOU A REGISTERED STUDENT? (YES/NO)")
-    if existingornot=="YES":
+    if existingornot.upper()=="YES":
         stud_ID=int(input("KINDLY ENTER YOUR STUDENT_ID PLEASE:- "))
         #TODO QUERY TO VERIFY THE AVAILIBILITY OF THE STUDENT IN THE DATABASE
         #TODO IF THE STUDENT DOESN'T EXIST ASK THE STUDENT TO EITHER REGISTER OR LEAVE
-    elif existingornot=="NO":
+    elif existingornot.upper()=="NO":
         registerornot_=input("DO YOU WANT TO REGISTER YOUR RECORDS IN THIS LIBRARY,")
-        if registerornot_=="YES":
+        if registerornot_.upper()=="YES":
             #TODO QUERY TO ADD THE STUDENT INTO THE DATABASE AND UPADATE THE TABLE STUDENT
             pass
-        elif registerornot_=="NO":
+        elif registerornot_.upper()=="NO":
             print("EITHER REGISTER INTO YOURSELF INTO THE DATABASE OR YOU WON'T BE ABLE TO ACCESS THE LIBRARY")
             #TODO ADD AN OPTION TO GO BACK TO THE REGISTERING PART
         else:
